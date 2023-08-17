e9af162beb04f01fe162158cf181fde63b17667f
user <2000-dimk@ukr.net>
2023-08-17 08:19:57 +0300

    Последний фикс

---
+++ src/main/java/org/example/config/CustomAuthenticationSuccessHandler.java
@@ -0,0 +1,35 @@
+package org.example.config;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+
+import java.io.IOException;
+import java.util.Collection;
+
+public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+
+
+
+    private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        for (GrantedAuthority authority : authorities) {
+            if (authority.getAuthority().equals("ADMIN") || authority.getAuthority().equals("MODERATOR")) {
+                redirectStrategy.sendRedirect(request, response, "/admin");
+                return;
+            }
+        }
+        redirectStrategy.sendRedirect(request, response, "/");
+    }
+}
+
